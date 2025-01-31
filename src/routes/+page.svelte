d3cad8c856f24f614b7f6318882aa95ce5dc4016
Dawit Mekonnen <56738450+Dave-lab12@users.noreply.github.com>
2025-01-31 13:06:33 +0300

    Ci debugging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import AboutUs from '../lib/ui/sections/About.svelte';
-	import Hero from '../lib/ui/sections/Hero.svelte';
+	import AboutUs from '$lib/ui/sections/about.svelte';
+	import Hero from '$lib/ui/sections/Hero.svelte';
 	import Featured from '$lib/ui/sections/Featured.svelte';
 	import Pricing from '$lib/ui/sections/Pricing.svelte';
 	import Faq from '$lib/ui/sections/Faq.svelte';
